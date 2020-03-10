ed511bddc25e68bbe642978d7a5d0e65efb4daeb
Mushfeq <mushfeq.khan@gmail.com>
2020-03-10 14:19:30 -0400

    Removing templates and views.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,18 +3,16 @@ from config import Config
 from app.database import db
 from app.serialization import ma
 from flask_migrate import Migrate
-from app.routes import handlers
 from app.api import api
 
-import logging
-logging.basicConfig()
-logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+# import logging
+# logging.basicConfig()
+# logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(Config)
-    app.register_blueprint(handlers)
     app.register_blueprint(api, url_prefix='/api')
     db.init_app(app)
     ma.init_app(app)
