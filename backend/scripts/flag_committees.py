00c75170ff6697a224ed98fd9da3391e319e690d
Mushfeq <mushfeq.khan@gmail.com>
2020-03-11 19:44:11 -0400

    Fixing flagging scripts.

--- backend/scripts/flag_committees.py
+++ backend/scripts/flag_committees.py
@@ -11,15 +11,15 @@ for employer in flagged_employers:
     print(f'Applying rules for {employer.name}...')
     for matching_rule in employer.matching_rules:
         print(f'Employer: {matching_rule.employer} City: {matching_rule.city} State: {matching_rule.state}')
-        query = IndividualContributor.query.filter(
-            IndividualContributor.employer.op('~')(matching_rule.employer))
+        query = Committee.query.filter(
+            Committee.name.op('~')(matching_rule.employer) | Committee.connected_organization.op('~')(matching_rule.employer))
         if matching_rule.city:
-            query = query.filter(IndividualContributor.city.op('~')(matching_rule.city))
+            query = query.filter(Committee.city.op('~')(matching_rule.city))
         if matching_rule.state:
-            query = query.filter(IndividualContributor.state == matching_rule.state)
+            query = query.filter(Committee.state == matching_rule.state)
 
         num_updated = query.update(
-            {IndividualContributor.employer_flagged_as_id: employer.id}, synchronize_session=False)
+            {Committee.connected_organization_flagged_as_id: employer.id}, synchronize_session=False)
         print(f'Matched: {num_updated}')
 
 db.session.commit()
