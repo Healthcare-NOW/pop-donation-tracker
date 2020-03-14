1d48acd6b8a9e51abdb4a99cdc3341223578c691
Mushfeq <mushfeq.khan@gmail.com>
2020-03-14 06:58:31 -0400

    Adding Dockerfile.

--- backend/production.py
+++ backend/production.py
@@ -1,7 +1,11 @@
 from app import create_app
 from config import Production
+import logging
 
 app = create_app(Production)
+
+gunicorn_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers = gunicorn_logger.handlers
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
