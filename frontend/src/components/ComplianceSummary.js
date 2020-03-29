fbba482417f27e5b61514adfa2883c7e529ac221
Mushfeq <mushfeq.khan@gmail.com>
2020-03-29 14:51:17 -0400

    Changing text.

--- frontend/src/components/ComplianceSummary.js
+++ frontend/src/components/ComplianceSummary.js
@@ -1,43 +1,45 @@
 import React from 'react';
 import {isEmpty, sumBy} from 'lodash';
-import {Icon, Message} from "semantic-ui-react";
+import {List, Message} from "semantic-ui-react";
 import {currencyFormat} from "../constants";
 
 export const ComplianceSummary = ({flaggedIndividualContributions, flaggedCommitteeContributions}) => {
     const totalCommitteeContributions = currencyFormat.format(sumBy(flaggedCommitteeContributions, 'amount'));
     return (
         <Message>
-            <div className="App-complianceSummaryItem">
-                {
-                    isEmpty(flaggedCommitteeContributions) ?
-                        (<span>
-                            <Icon name="check circle" color="green" size="large"/> This candidate has not received any
-                            contributions exceeding $200 from flagged committees.
-                        </span>)
-                        :
-                        (<span>
-                            <Icon name="warning circle" color="red" size="large"/> This candidate has received
-                            contributions totalling {totalCommitteeContributions} from flagged committees.
-                        </span>)
+            <List relaxed>
+                <List.Item>
+                    {
+                        isEmpty(flaggedCommitteeContributions) ?
+                            (<><List.Icon name="check circle" color="green" size="large"/>
+                                <List.Content>
+                                    No contributions exceeding $200 from flagged committees.
+                                </List.Content></>)
+                            :
+                            (<><List.Icon name="warning circle" color="red" size="large"/>
+                                <List.Content>Received contributions totalling {totalCommitteeContributions} from flagged committees.
+                                </List.Content></>)
+                    }
+                </List.Item>
+                <List.Item>
+                    {
+                        isEmpty(flaggedIndividualContributions) ?
+                            (<><List.Icon name="check circle" color="green" size="large"/>
+                                <List.Content>No contributions exceeding $200 from employees of flagged companies.</List.Content></>)
+                            :
+                            (<>
+                                <List.Icon name="warning circle" color="orange" size="large"/>
+                                <List.Content>
+                                    Received contributions exceeding $200 from employees of flagged
+                                        companies.
+                                    <p><i>Note: These employees may or may not be high-ranking
+                                        executives.</i></p>
+                                </List.Content>
+                            </>)
 
-                }
-            </div>
-            <div>
-            {
-                isEmpty(flaggedIndividualContributions) ?
-                    <span>
-                        <Icon name="check circle" color="green" size="large"/> This candidate has not received any
-                        contributions exceeding $200 from employees of flagged companies.
-                    </span>
-                    :
-                    <span>
-                        <Icon name="warning circle" color="orange" size="large"/> This candidate has received
-                        contributions exceeding $200 from employees of flagged committees. <i>Note: These employees
-                        may or may not be high-ranking executives.</i>
-                    </span>
-
-            }
-            </div>
+                    }
+                </List.Item>
+            </List>
         </Message>
     );
 };
